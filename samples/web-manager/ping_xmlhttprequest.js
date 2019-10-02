64a2574ebf3977aceb662f8aadf6044c5efe48f3
Germano Massullo <germano.massullo@cern.ch>
2019-10-02 17:19:45 +0200

    .

--- samples/web-manager/ping_xmlhttprequest.js
+++ samples/web-manager/ping_xmlhttprequest.js
@@ -1,16 +1,21 @@
 function XMLHttpRequest_test()
 {
     var request = new XMLHttpRequest();
-    //var response = new XMLHttpRequest();
-    request.open("POST", "http://127.0.0.1:31416",false);
+    request.open("POST", "http://127.0.0.1:31416"); // with false it triggers network error
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     request.send("<boinc_gui_rpc_request><exchange_versions><major>BOINC_MAJOR_VERSION</major><minor>BOINC_MINOR_VERSION</minor><release>BOINC_RELEASE</release></exchange_versions></boinc_gui_rpc_request>");
-    if(request.status !== 200) throw new Error(request.statusText);
-    var type = request.getResponseHeader("Content-Type")
-    if(!type.match(/^text/))
-	{
-		throw new Error("Expected textual response; got: " + type);
-	}
-	console.log(request);
+    /* I don't know how this function can iterate without a for cycle.
+     * The code is based on some examples from book "JavaScript the definitive guide"
+     */
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === 4 && request.status === 200)
+        {
+            var type = request.getResponseHeader("response");
+            console.log(type);
+            console.log(request);
+            alert(request.responseText);
+        }
+    }
 } 
-
 
